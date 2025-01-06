fee1b46af6081b10b1d4f3dec00d17da6d331f24
KyawThetHtwe5502 <kyawthethtwe5502@gmail.com>
2025-01-06 16:54:26 +0630

    start to build audio-test

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { StreamCall, StreamVideo, StreamVideoClient, User } from '@stream-io/video-react-sdk';
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+
+const apiKey = 'mmhfdzb5evj2';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0x1a2VfU2t5d2Fsa2VyIiwidXNlcl9pZCI6Ikx1a2VfU2t5d2Fsa2VyIiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NjA0ODAwLCJpYXQiOjE3MzYxNTc4OTIsImV4cCI6MTczNjc2MjY5Mn0.gO4k6amSS53lBVL7TkP70uynqqAJScisuNTNx7I8ePY';
+const userId = 'Luke_Skywalker';
+const callId = 'pRu3P4YnSJT7';
+// initialize the user object
+const user: User = {
+  id: userId,
+  name: 'Oliver',
+  image: 'https://getstream.io/random_svg/?id=oliver&name=Oliver',
+};
+
+const client = new StreamVideoClient({ apiKey, user, token });
+const call = client.call('audio_room', callId);
+call.join({
+  create: true,
+  data: {
+    members: [/* { user_id: 'john_smith' }, { user_id: 'jane_doe' } */],
+    custom: {
+      title: 'React Rooms',
+      description: 'Talking about React',
+    },
+  },
+});
+
+export default function App() {
+  return (
+    <StreamVideo client={client}>
+      <StreamCall call={call}>
+        {/** We will introduce the <MyUILayout /> component later */}
+        {/** <MyUILayout /> */}
+      </StreamCall>
+    </StreamVideo>
+  );
+};
