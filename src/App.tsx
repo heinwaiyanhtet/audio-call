cfe1541b91a2ecc8f76f7e07e95c7d39efec60ba
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2025-03-27 21:22:55 +0630

    main

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import '@stream-io/video-react-sdk/dist/css/styles.css';
 import { MyUILayout } from './components/MyUILayout';
 
 const apiKey = 'mmhfdzb5evj2';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0x1bWluYXJhX1VuZHVsaSIsInVzZXJfaWQiOiJMdW1pbmFyYV9VbmR1bGkiLCJ2YWxpZGl0eV9pbl9zZWNvbmRzIjo2MDQ4MDAsImlhdCI6MTc0MzAwNjI0OSwiZXhwIjoxNzQzNjExMDQ5fQ.-R32BlSjJxZs0iZmbjgR411cuMQMvufcjpS6kOKBfnE';
-const userId = 'Luminara_Unduli';
-const callId = 'eLmvVULpSnlc';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0NhcHRhaW5fUmV4IiwidXNlcl9pZCI6IkNhcHRhaW5fUmV4IiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NjA0ODAwLCJpYXQiOjE3NDMwODcwNDIsImV4cCI6MTc0MzY5MTg0Mn0.cN0digMf_-Dyqb0amMbA7Xz2iuUAXsXi8oA6VJNtm9Q';
+const userId = 'Captain_Rex';
+const callId = '47TKFoL07tnR';
 // initialize the user object
 const user: User = {
   id: userId,
   name: 'Oliver',
   image: 'https://getstream.io/random_svg/?id=oliver&name=Oliver',
 };
+
 
 const client = new StreamVideoClient({ apiKey, user, token });
 const call = client.call('audio_room', callId);
