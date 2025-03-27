88e3c822352a78d59dd49ce7881c3d3afd7e9d67
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2025-03-27 22:02:02 +0630

    main

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import '@stream-io/video-react-sdk/dist/css/styles.css';
 import { MyUILayout } from './components/MyUILayout';
 
 const apiKey = 'mmhfdzb5evj2';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0NhcHRhaW5fUmV4IiwidXNlcl9pZCI6IkNhcHRhaW5fUmV4IiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NjA0ODAwLCJpYXQiOjE3NDMwODcwNDIsImV4cCI6MTc0MzY5MTg0Mn0.cN0digMf_-Dyqb0amMbA7Xz2iuUAXsXi8oA6VJNtm9Q';
-const userId = 'Captain_Rex';
-const callId = '47TKFoL07tnR';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0FuYWtpbl9Tb2xvIiwidXNlcl9pZCI6IkFuYWtpbl9Tb2xvIiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NjA0ODAwLCJpYXQiOjE3NDMwODg4NTYsImV4cCI6MTc0MzY5MzY1Nn0.m3fOYXOmPnnxPp3l9-dOD-scl0fpFGfmnzRx7W5yb9Y';
+const userId = 'Anakin_Solo';
+const callId = '6zClVdBwcjIV';
 // initialize the user object
 const user: User = {
   id: userId,
