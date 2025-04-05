ccf7b3fa90dc2583b1dd2c3fd6d48df07608c4bb
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2025-04-05 22:54:38 +0630

    audio-room

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import {
 
 const apiKey = "fhadftta5n4v";
 const userId = "679d7e025c89002b3a988049";
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjc5ZDdlMDI1Yzg5MDAyYjNhOTg4MDQ5IiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6MzYwMCwiaWF0IjoxNzQzODY4MTM4LCJleHAiOjE3NDM4NzE3Mzh9.DBUDEiW1mKDjDDCikxyyfRpQB8_4R3r_xP36iQ9hTyw";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjc5ZDdlMDI1Yzg5MDAyYjNhOTg4MDQ5IiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6MzYwMCwiaWF0IjoxNzQzODcwMjYzLCJleHAiOjE3NDM4NzM4NjN9.DVyPuDUGzMD0yOQrpKb1x163FJ1m95kd_rXKRVtWokc";
 const user = 
 { 
     id: userId
@@ -29,7 +29,7 @@ const client = new StreamVideoClient(
 
    }
 );
-const call = client.call("default", "my-first-call");
+const call = client.call("audio_room", "my-first-call");
 call.join({ create: true });
 
 export default function MyApp  ()  {
