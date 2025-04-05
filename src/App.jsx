97eb9b0d3694973bf0df05f73cb23fc1104aac01
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2025-04-05 21:13:11 +0630

    audio

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import '@stream-io/video-react-sdk/dist/css/styles.css';
 import { MyUILayout } from './components/MyUILayout';
 
 const apiKey = 'mmhfdzb5evj2';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0dlbmVyYWxfR3JpZXZvdXMiLCJ1c2VyX2lkIjoiR2VuZXJhbF9Hcmlldm91cyIsInZhbGlkaXR5X2luX3NlY29uZHMiOjYwNDgwMCwiaWF0IjoxNzQzNDM4NDM5LCJleHAiOjE3NDQwNDMyMzl9.pQP6_1Bu39esGSWfF1aO5te0Gcc4Mqm1svmeixvoNdc';
-const userId = 'General_Grievous';
-const callId = 'D7UlHG2F7CcE';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0RlbmdhciIsInVzZXJfaWQiOiJEZW5nYXIiLCJ2YWxpZGl0eV9pbl9zZWNvbmRzIjo2MDQ4MDAsImlhdCI6MTc0Mzg2MjE4MiwiZXhwIjoxNzQ0NDY2OTgyfQ.snyQvj6g6TToAWrDzEHNB0LG3ZmBVLFwPY2g-1rJeh0';
+const userId = 'Dengar';
+const callId = 'JM0L0DqicbRf';
 // initialize the user object
 const user = {
   id: userId,
@@ -14,11 +14,12 @@ const user = {
 };
 
 const client = new StreamVideoClient({ apiKey, user, token });
+
 const call = client.call('audio_room', callId);
 call.join({
   create: true,
+  members: [{ user_id: 'john_smith' }, { user_id: 'jane_doe' }],
   data: {
-    // members: [ { user_id: 'john_smith' }, { user_id: 'jane_doe' } ],
     custom: {
       title: 'React Rooms',
       description: 'Talking about React',
@@ -30,7 +31,6 @@ export default function App() {
   return (
     <StreamVideo client={client}>
       <StreamCall call={call}>
-        {/** We will introduce the <MyUILayout /> component later */}
         <MyUILayout />
       </StreamCall>
     </StreamVideo>
