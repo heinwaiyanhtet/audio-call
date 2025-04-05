d1573cd6ff512733b99a222e232cb8b3f3d4cc60
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2025-04-05 23:05:57 +0630

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import {
   CallControls,
   SpeakerLayout,
@@ -7,55 +8,47 @@ import {
   StreamVideoClient,
 } from "@stream-io/video-react-sdk";
 
-
 const apiKey = "fhadftta5n4v";
 const userId = "679d7e025c89002b3a988049";
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjc5ZDdlMDI1Yzg5MDAyYjNhOTg4MDQ5IiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6MzYwMCwiaWF0IjoxNzQzODcwMjYzLCJleHAiOjE3NDM4NzM4NjN9.DVyPuDUGzMD0yOQrpKb1x163FJ1m95kd_rXKRVtWokc";
-const user = 
-{ 
-    id: userId
-};
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjc5ZDdlMDI1Yzg5MDAyYjNhOTg4MDQ5IiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6MzYwMCwiaWF0IjoxNzQzODcwMjYzLCJleHAiOjE3NDM4NzM4NjN9.DVyPuDUGzMD0yOQrpKb1x163FJ1m95kd_rXKRVtWokc"; 
 
-const client = new StreamVideoClient(
-  { apiKey, user, token,
-    options: {
-      maxConnectUserRetries: 3,
-      onConnectUserError: (err, allErrors) => {
+const user = { id: userId };
 
-        console.error("Failed to connect user", err, allErrors);
-     
-      },
-    },    
+const client = new StreamVideoClient({
+  apiKey,
+  user,
+  token,
+  options: {
+    maxConnectUserRetries: 3,
+    onConnectUserError: (err, allErrors) => {
+      console.error("Failed to connect user", err, allErrors);
+    },
+  },
+});
 
-   }
-);
 const call = client.call("audio_room", "my-first-call");
-await call.camera.disable(); 
-call.join(
-    { create: true });
 
-export default function MyApp  ()  {
+export default function MyApp() {
+  useEffect(() => {
+    const setupCall = async () => {
+      try {
+        await call.camera.disable(); // Disable camera before joining
+        await call.join({ create: true });
+      } catch (err) {
+        console.error("Error setting up call:", err);
+      }
+    };
+    setupCall();
+  }, []);
+
   return (
     <StreamVideo client={client}>
       <StreamCall call={call}>
-          <StreamTheme>
-            <SpeakerLayout />
-            <CallControls />
-          </StreamTheme>
+        <StreamTheme>
+          <SpeakerLayout />
+          <CallControls />
+        </StreamTheme>
       </StreamCall>
-    </StreamVideo> 
+    </StreamVideo>
   );
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
