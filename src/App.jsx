efff278e44e666b39a565b9f6fe89e3789aa5d5b
Hein Wai Yan Htet <85125630+heinwaiyanhtet@users.noreply.github.com>
2025-05-19 20:57:59 +0630

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,62 @@ import {
 const apiKey = "fhadftta5n4v";
 const userId = "67a818e525b4c0eff6ce55a7";
 // const userId = "679e5214e163890ef06ead3b";
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjdhODE4ZTUyNWI0YzBlZmY2Y2U1NWE3IiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6MzYwMCwiaWF0IjoxNzQ3NjYzNzIwLCJleHAiOjE3NDc2NjczMjB9.cRKILj_kMz_UMBXHH7wDvKunlfso5DPxzG_n0xVrPfc"; 
+const token = "import React, { useEffect } from "react";
+import {
+  CallControls,
+  SpeakerLayout,
+  StreamCall,
+  StreamTheme,
+  StreamVideo,
+  StreamVideoClient,
+} from "@stream-io/video-react-sdk";
+
+
+const apiKey = "fhadftta5n4v";
+const userId = "67a818e525b4c0eff6ce55a7";
+// const userId = "679e5214e163890ef06ead3b";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjdhODE4ZTUyNWI0YzBlZmY2Y2U1NWE3IiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6MzYwMCwiaWF0IjoxNzQ3NjY0NDc5LCJleHAiOjE3NDc2NjgwNzl9.uxZkj76GUad3q2FwNc7dNQ97YbpW-gGC2EUqcrzO-XA"; 
+
+const user = { id: userId };
+
+
+const client = new StreamVideoClient({
+  apiKey,
+  user,
+  token,
+  options: {
+    maxConnectUserRetries: 3,
+    onConnectUserError: (err, allErrors) => {
+      console.error("Failed to connect user", err, allErrors);
+    },
+  },
+});
+
+const call = client.call("audio_room", "call-1747664480569");
+export default function MyApp() {
+  useEffect(() => {
+    const setupCall = async () => {
+      try {
+        await call.camera.disable(); 
+        await call.join({ create: true });
+      } catch (err) {
+        console.error("Error setting up call:", err);
+      }
+    };
+    setupCall();
+  }, []);
+
+  return (
+    <StreamVideo client={client}>
+      <StreamCall call={call}>
+        <StreamTheme>
+          <SpeakerLayout />
+          <CallControls />
+        </StreamTheme>
+      </StreamCall>
+    </StreamVideo>
+  );
+}"; 
 
 const user = { id: userId };
 
